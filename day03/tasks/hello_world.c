90fe2d3ab252279cf12f03a191b61677bdae546b
Jonns Mostovoys <amarr.industrial@gmail.com>
2013-08-08 23:47:31 +0300

    Add English translation.

--- day03/tasks/hello_world.c
+++ day03/tasks/hello_world.c
@@ -5,10 +5,18 @@
  * и возвращает 0 — код успешного выполнения.
  *
  * Задание:
- * Возвоащайте 5 — код ошибки если функция printf
+ * Возвращайте 5 — код ошибки ввода/вывода 
+ * в случае, если функция printf
  * не отработала корректно. Для подсказок при решении
  * этого задания используйте Google и файл 
  * day03/task/hint.txt (в таком порядке).
+ 
+ * This is program that prints “Hello, World”
+ *
+ * Task:
+ * and returns 5 — I/O error code if printf didn't
+ * work correctly. For hints see Google and 
+ * day03/task/hint.txt (in that order).
  */
 
 int main() {
